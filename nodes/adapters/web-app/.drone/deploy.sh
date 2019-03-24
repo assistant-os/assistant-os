894ca1b731fe6f30482777b2ebd45fbcdaa4b7ae
Thibault Friedrich <thibault.friedrich@gmail.com>
2019-03-24 15:52:35 +0100

    change way to add ssh key

--- nodes/adapters/web-app/.drone/deploy.sh
+++ nodes/adapters/web-app/.drone/deploy.sh
@@ -13,5 +13,6 @@ git remote add deploy git@github.com:assistant-os/assistant-os.github.io.git
 
 echo $GIT_PUSH_SSH_KEY > .ssh_key
 
-ssh-agent sh -c 'ssh-add .ssh_key;
+ssh-agent
+ssh-add .ssh_key
 git push -f deploy master
