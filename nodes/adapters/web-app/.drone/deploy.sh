87b94048deb842fb9fda4e32792047f801d3e4bd
Thibault Friedrich <thibault.friedrich@gmail.com>
2019-03-24 15:31:43 +0100

    add email

--- nodes/adapters/web-app/.drone/deploy.sh
+++ nodes/adapters/web-app/.drone/deploy.sh
@@ -4,7 +4,13 @@ echo $GIT_PUSH_SSH_KEY > .ssh_key
 
 cd build
 
+
+
 git init
+
+git config user.email "thibault.friedrich@gmail.com"
+git config user.name "Thibault Friedrich"
+
 git add .
 git commit -am "new files"
 git remote add deploy git@github.com:assistant-os/assistant-os.github.io.git
