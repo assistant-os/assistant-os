27961d51a69f9813192e619b11754c5eca2f8b26
Thibault Friedrich <thibault.friedrich@gmail.com>
2019-03-24 16:03:44 +0100

    change way to add ssh key

--- nodes/adapters/web-app/.drone/deploy.sh
+++ nodes/adapters/web-app/.drone/deploy.sh
@@ -13,6 +13,11 @@ git remote add deploy git@github.com:assistant-os/assistant-os.github.io.git
 
 echo $GIT_PUSH_SSH_KEY > .ssh_key
 
-ssh-agent
-ssh-add .ssh_key
+echo "Host github.com" > ~/.ssh/config
+echo "  User git" >> ~/.ssh/config
+echo "  Hostname github.com" >> ~/.ssh/config
+echo '  IdentityFile $PWD/.ssh_key' >> ~/.ssh/config
+
+# ssh-agent
+# ssh-add .ssh_key
 git push -f deploy master
