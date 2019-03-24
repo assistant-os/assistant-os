3fd7e1a82e9e7a8b9184e9cf7db08b97346b2914
Thibault Friedrich <thibault.friedrich@gmail.com>
2019-03-24 17:22:49 +0100

    use previous approach

--- nodes/adapters/web-app/.drone/deploy.sh
+++ nodes/adapters/web-app/.drone/deploy.sh
@@ -13,22 +13,15 @@ git remote add deploy git@github.com:assistant-os/assistant-os.github.io.git
 
 echo $GIT_PUSH_SSH_KEY > .ssh_key
 
-chmod 700 .ssh_key
+mkdir ~/.ssh
+touch ~/.ssh/config
+echo "Host github.com" > ~/.ssh/config
+echo "  User git" >> ~/.ssh/config
+echo "  Hostname github.com" >> ~/.ssh/config
+echo '  IdentityFile $PWD/.ssh_key' >> ~/.ssh/config
 
-ssh-agent -k sh -c "ssh-add -k $PWD/.ssh_key; git push -f deploy master"
+cat $
 
-
-
-# https://help.github.com/en/articles/error-permission-denied-publickey
-
-# mkdir ~/.ssh
-# touch ~/.ssh/config
-# echo "Host github.com" > ~/.ssh/config
-# echo "  User git" >> ~/.ssh/config
-# echo "  Hostname github.com" >> ~/.ssh/config
-# echo "  IdentityFile $PWD/.ssh_key" >> ~/.ssh/config
-
-
-# https://superuser.com/questions/232373/how-to-tell-git-which-private-key-to-use
-
-# GIT_SSH_COMMAND="ssh -i $PWD/.ssh_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push -f deploy master
+ssh-agent
+# ssh-add .ssh_key
+git push -f deploy master
